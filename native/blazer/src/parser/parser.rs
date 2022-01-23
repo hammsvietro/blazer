197ab0a07fab48e35e261c95bc473ec3d71fff23
pedro henrique hamms vietro <pedro hamms>
2022-01-23 20:17:27 -0300

    successfully parses a map

--- native/blazer/src/parser/parser.rs
+++ native/blazer/src/parser/parser.rs
@@ -1,24 +1,41 @@
 use core::panic;
-
 use rustler::Term;
+use std::mem;
+use convert_case::{Case, Casing};
 
-pub fn return_for_map<'a>(map: &Term<'a>) -> Vec<String> {
+pub fn return_for_map<'a>(map: &Term<'a>) -> Term<'a> {
 	let iterator = rustler::MapIterator::new(*map).unwrap(); 
+    let env = Term::get_env(&map);
 
-	let term = Term::get_env(map);
-	let map = Term::map_new(term);
-	Term::map_put(map, "zas", 123);
+	let (keys, values): (Vec<Term<'a>>, Vec<Term<'a>>) = iterator.map(|(key, value)| {
+		let transformed_key = match key.get_type() {
+			rustler::TermType::Atom => panic!("can't parse any other type"),
+			rustler::TermType::Binary  => parse_string(key),
+			_ => panic!("can't parse any other type")
+		};
 
-	iterator.map(|(key, _value)| {
-		match key.get_type() {
-			rustler::TermType::Atom => rustler::Term::atom_to_string(&key).unwrap(),
-			rustler::TermType::Binary  => binary_term_to_string(&key),
-			_ => panic!("can't any other type")
-		}
+        (transformed_key, value)
 
-	}).collect::<Vec<String>>()
+	}).unzip();
+    Term::map_from_arrays(env, &keys.as_slice(), &values.as_slice()).unwrap()
 }
 
+
+fn parse_atom(term: Term) -> () {
+
+}
+
+fn parse_string(term: Term) -> Term {
+    let env = Term::get_env(&term);
+
+    let string = binary_term_to_string(&term);
+    let parsed_string = string.to_case(Case::Camel);
+    let string_size = parsed_string.as_bytes();
+    let mut owned_binary = rustler::OwnedBinary::new(string_size.len()).unwrap();
+    owned_binary.as_mut_slice().copy_from_slice(string_size);
+
+    rustler::Binary::from_owned(owned_binary, env).to_term(env)
+}
 
 fn binary_term_to_string<'a>(term: &Term<'a>) -> String {
 	let binary: rustler::Binary = match term.into_binary() {
@@ -26,5 +43,4 @@ fn binary_term_to_string<'a>(term: &Term<'a>) -> String {
 		Err(_) => panic!("cannot parse binary key")
 	};
 	String::from_utf8_lossy(binary.as_slice()).into_owned()
-
 }
