7a0f10d9f6f016f5e7a9584e0e03cc6b8559f7c8
hammsvietro <hammsvietro@gmail.com>
2022-02-11 01:11:54 -0300

    adds config parser

--- native/blazer/src/model/options.rs
+++ native/blazer/src/model/options.rs
@@ -1,6 +1,6 @@
-use rustler::Term;
-use super::super::parser::string::string_to_term;
 use super::super::cases::case::get_case;
+use super::super::parser::string::string_to_term;
+use rustler::Term;
 
 pub enum Keys {
   Atoms,
@@ -9,56 +9,52 @@ pub enum Keys {
 
 pub struct BlazerOptions {
   case: convert_case::Case,
-  keys: Keys
+  keys: Keys,
 }
 
 impl BlazerOptions {
   pub fn from_term<'a>(term: Term<'a>) -> Result<BlazerOptions, Term<'a>> {
     let mut options = BlazerOptions {
       keys: Keys::Strings,
-      case: convert_case::Case::Camel
+      case: convert_case::Case::Camel,
     };
 
     let env = term.get_env();
 
     let list_iterator: Vec<(Term<'a>, Term<'a>)> = match term.decode() {
-        Ok(iterator) => iterator,
-        Err(_) => return Err(string_to_term("couldn't iterate through opts".into(), env))
+      Ok(iterator) => iterator,
+      Err(_) => return Err(string_to_term("couldn't iterate through opts".into(), env)),
     };
     for (key, value) in list_iterator.into_iter() {
-      let string_key = atom_to_string(&key);
+      let string_key = atom_to_string(&key)?;
       match &string_key[..] {
-        "case" => {
-            options.case = match get_case(&value) {
-                Ok(case) => case,
-                Err(err) => return Err(err)
-            };
-        },
-        "keys" => {
-            options.keys = match resolve_keys(&value) {
-                Ok(keys) => keys,
-                Err(error) => return Err(error)
-            };
-        },
-        _ => {},
+        "case" => options.case = get_case(&value)?,
+        "keys" => options.keys = resolve_keys(&value)?,
+        _ => (),
       };
-    };
+    }
     Ok(options)
   }
 }
 
-fn atom_to_string<'a>(term: &Term<'a>) -> String {
-    Term::atom_to_string(term).unwrap()
+fn atom_to_string<'a>(term: &Term<'a>) -> Result<String, Term<'a>> {
+  match Term::atom_to_string(term) {
+    Ok(string) => Ok(string),
+    Err(_) => Err(string_to_term(
+      "couldn't parse atom into string".into(),
+      term.get_env(),
+    )),
+  }
 }
 
 fn resolve_keys<'a>(term: &Term<'a>) -> Result<Keys, Term<'a>> {
-    let string_value: String = atom_to_string(term);
-    match &string_value[..] {
-      "atoms"|"atoms!" => Ok(Keys::Atoms),
-      "strings" => Ok(Keys::Strings),
-      _ => {
-        let env = term.get_env();
-        Err(string_to_term("invalid :keys option".into(), env))
-      }
-    }
+  let string_value: String = atom_to_string(term)?;
+  match &string_value[..] {
+    "atoms" | "atoms!" => Ok(Keys::Atoms),
+    "strings" => Ok(Keys::Strings),
+    _ => Err(string_to_term(
+      "invalid :keys option".into(),
+      term.get_env(),
+    )),
+  }
 }
