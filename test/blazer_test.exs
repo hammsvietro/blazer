e0468dbf2b512a0b2e9a97a1815fa678d3d6c07c
hammsvietro <hammsvietro@gmail.com>
2022-02-11 02:22:22 -0300

    fixes tests

--- test/blazer_test.exs
+++ test/blazer_test.exs
@@ -5,14 +5,14 @@ defmodule BlazerTest do
   describe "parse/2" do
     test "should return {:error, reason} when the target case isn't valid" do
       input = %{input_case: true}
-      return = Blazer.parse(input, :invalid_case)
+      return = Blazer.parse(input, case: :invalid_case)
       assert {:error, reason} = return
       assert is_binary(reason)
     end
 
     test "should return {:ok, result} when the the input is valid" do
       input = %{input_case: true}
-      return = Blazer.parse(input, :camel)
+      return = Blazer.parse(input, case: :camel)
       assert {:ok, output_map} = return
       assert %{"inputCase" => true} = output_map
     end
